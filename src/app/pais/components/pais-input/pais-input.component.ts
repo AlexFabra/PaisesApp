b2bcc03fbc7e05a07f6f6fae21d3df3ef81a9885
AlexFabra <alfaseart@gmail.com>
2021-11-04 11:14:35 +0100

    mostrar sugerencias preimplementado

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -12,15 +12,18 @@ export class PaisInputComponent implements OnInit {
   
   // declaramos Output para emitir el termino introducido por el usuario, lo cual haremos en buscar()
   @Output() onEnter   : EventEmitter<string> = new EventEmitter();
+  //se va a emitir cuando la persona deje de escribir
   @Output() onDebounce: EventEmitter<string> = new EventEmitter();
   @Input() placeholder: string = '';
-  //el debounce se emitirá cuando yo deje de escribir:
+  //el debounce se emitirá cuando yo deje de escribir (subject es un observable especial de rxjs):
   debouncer: Subject<string> = new Subject();
 
   termino: string = '';
   //se dispara cuando el componente es creado:
   ngOnInit() {
     //cuando el debouncer deja de recibir valores por 300ms, entonces emite el valor.
+    //pipe es una conexion que me permite transformar la salida del subscribe
+    //debounceTime
     this.debouncer
       .pipe(debounceTime(300))
       .subscribe( valor => {
@@ -34,6 +37,7 @@ export class PaisInputComponent implements OnInit {
   }
 
   teclaPresionada() {
+    //emitimos el valor cada vez que el usuario presiona una tecla mediante el evento que activa esta funcion.
     this.debouncer.next( this.termino );
     // se puede mostrar a tiempo real el termino que se está recibiendo: console.log(this.termino)
   }
